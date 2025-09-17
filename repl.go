0e21f5d0b6f2543ae8296e3ab12b074cd63a71dd
gooneraki <hayiate4@hotmail.com>
2025-09-17 17:08:10 +0300

    Refactor REPL command handling to use structured command definitions and improve command execution flow

--- repl.go
+++ repl.go
@@ -7,20 +7,62 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("Bla bla bla")
+	return nil
+}
+
 func startRepl() {
+	COMMANDS := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+
 	reader := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
 		reader.Scan()
 
-		words := cleanInput(reader.Text())
-		if len(words) == 0 {
-			continue
+		fullCommand := reader.Text()
+
+		commandStruct, found := COMMANDS[fullCommand]
+
+		if found {
+			commandStruct.callback()
+		} else {
+			fmt.Println("Unknown command")
 		}
 
-		commandName := words[0]
+		// words := cleanInput(fullCommand)
+		// if len(words) == 0 {
+		// 	continue
+		// }
 
-		fmt.Printf("Your command was: %s\n", commandName)
+		// commandName := words[0]
+
+		// fmt.Printf("Your command was: %s\n", commandName)
 	}
 }
 
