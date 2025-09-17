8ad28b552f5a4c5399a81164c581dbf7edc85b09
gooneraki <hayiate4@hotmail.com>
2025-09-17 16:45:44 +0300

    SOLUTION: Refactor main function to start REPL directly and remove redundant input handling code

--- main.go
+++ main.go
@@ -1,32 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-
-	userInput := ""
-	scanner := bufio.NewScanner(os.Stdin)
-	firstWord := ""
-
-	for {
-		fmt.Print("Pokedex > ")
-		if scanner.Scan() {
-			userInput = scanner.Text()
-		}
-		cleaned := cleanInput(userInput)
-		userInput = ""
-
-		if len(cleaned) > 0 {
-			firstWord = cleaned[0]
-		} else {
-			firstWord = ""
-		}
-
-		fmt.Printf("Your command was: %s\n", firstWord)
-
-	}
+	startRepl()
 }
