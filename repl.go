8d5ed5769bfe9350eabc2dbcbaa5d8f907a4e15b
gooneraki <hayiate4@hotmail.com>
2025-09-17 17:10:45 +0300

    SOLUTION -> Implement command handling by separating command logic into dedicated functions for exit and help

--- repl.go
+++ repl.go
@@ -7,62 +7,30 @@ import (
 	"strings"
 )
 
-type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
-}
-
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-func commandHelp() error {
-	fmt.Println("Welcome to the Pokedex!")
-	fmt.Println("Usage:")
-	fmt.Println("Bla bla bla")
-	return nil
-}
-
 func startRepl() {
-	COMMANDS := map[string]cliCommand{
-		"exit": {
-			name:        "exit",
-			description: "Exit the Pokedex",
-			callback:    commandExit,
-		},
-		"help": {
-			name:        "help",
-			description: "Displays a help message",
-			callback:    commandHelp,
-		},
-	}
-
 	reader := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
 		reader.Scan()
 
-		fullCommand := reader.Text()
-
-		commandStruct, found := COMMANDS[fullCommand]
-
-		if found {
-			commandStruct.callback()
-		} else {
-			fmt.Println("Unknown command")
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
 		}
 
-		// words := cleanInput(fullCommand)
-		// if len(words) == 0 {
-		// 	continue
-		// }
+		commandName := words[0]
 
-		// commandName := words[0]
-
-		// fmt.Printf("Your command was: %s\n", commandName)
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
 	}
 }
 
@@ -70,4 +38,25 @@ func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
