33243f5f4d29430fdeb99aaa13c1248689efdcaa
gooneraki <hayiate4@hotmail.com>
2025-09-17 16:21:32 +0300

    COPY SOLUTION - Add .gitignore, update module path, and refactor input cleaning logic

--- main.go
+++ main.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func cleanInput(text string) []string {
-
-	result := strings.Fields(text)
-	for i := 0; i < len(result); i++ {
-		result[i] = strings.ToLower(result[i])
-	}
-
-	return result
-}
+import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
-	text := "hey I JUST       met you"
-	fmt.Println(cleanInput(text))
 }
--- repl_test.go
+++ repl_test.go
@@ -10,33 +10,35 @@ func TestCleanInput(t *testing.T) {
 		expected []string
 	}{
 		{
+			input:    "  ",
+			expected: []string{},
+		},
+		{
+			input:    "  hello  ",
+			expected: []string{"hello"},
+		},
+		{
 			input:    "  hello  world  ",
 			expected: []string{"hello", "world"},
 		},
 		{
-			input:    " Oh THAT    is the spirit ",
-			expected: []string{"oh", "that", "is", "the", "spirit"},
+			input:    "  HellO  World  ",
+			expected: []string{"hello", "world"},
 		},
 	}
 
 	for _, c := range cases {
 		actual := cleanInput(c.input)
-
 		if len(actual) != len(c.expected) {
-			t.Errorf("Lengths of expected and actual results are not the same")
-			return
+			t.Errorf("lengths don't match: '%v' vs '%v'", actual, c.expected)
+			continue
 		}
-
 		for i := range actual {
 			word := actual[i]
 			expectedWord := c.expected[i]
-
 			if word != expectedWord {
-				t.Errorf("Different actual %s vs expected %s", word, expectedWord)
-				return
+				t.Errorf("cleanInput(%v) == %v, expected %v", c.input, actual, c.expected)
 			}
-
 		}
 	}
-
 }
